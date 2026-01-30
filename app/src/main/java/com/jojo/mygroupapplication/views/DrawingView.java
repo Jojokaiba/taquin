6b4668744776eacd3587b5df09623df1606f1032
Jojokaiba <mongjohanandry@gmail.com>
2026-01-30 15:43:07 +0300

    Conversion du taquin déssiné en ligne en taquin dessiné en carré

--- app/src/main/java/com/jojo/mygroupapplication/views/DrawingView.java
+++ app/src/main/java/com/jojo/mygroupapplication/views/DrawingView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,6 +19,9 @@ public class DrawingView extends View {
     float posY;
     private Paint drawingPaint;
     private Paint textPaint;
+
+
+
 
     public DrawingView(Context context) {
         super(context);
@@ -37,7 +41,7 @@ public class DrawingView extends View {
     private void initComponents(){
         drawingPaint = new Paint();
         drawingPaint.setColor(Color.BLACK);
-        drawingPaint.setStyle(Paint.Style.FILL);
+        drawingPaint.setStyle(Paint.Style.STROKE);
         drawingPaint.setStrokeWidth(10);   // Epaisseur de la ligne
         drawingPaint.setAntiAlias(true);
 
@@ -56,9 +60,11 @@ public class DrawingView extends View {
         float currentPositionX = getWidth() / 6;
         float currentPositionY = getHeight() / 2 ;
         int compteur = 1;
-        for (int i=1; i<5 ; i++){
-            for (int j=1; j<5 ; j++){
-                drawingCanvas.drawLine(currentPositionX, currentPositionY , currentPositionX + 200, currentPositionY, drawingPaint);  //premier côté du carré
+        Rect carre =  new Rect(200,200,200,200);
+        for (int i=1; i<4 ; i++){
+            for (int j=1; j<4 ; j++){
+
+                /* drawingCanvas.drawLine(currentPositionX, currentPositionY , currentPositionX + 200, currentPositionY, drawingPaint);  //premier côté du carré
                 currentPositionX += 200;
 
                 drawingCanvas.drawLine(currentPositionX, currentPositionY , currentPositionX, currentPositionY + 200, drawingPaint); // deuxième côté du carré
@@ -69,6 +75,10 @@ public class DrawingView extends View {
 
                 drawingCanvas.drawLine(currentPositionX, currentPositionY , currentPositionX, currentPositionY - 200, drawingPaint);
                 currentPositionY -= 200;
+
+                 */
+
+                drawingCanvas.drawRect(currentPositionX,currentPositionY,currentPositionX+200 , currentPositionY+200, drawingPaint);
 
                 if(compteur <= 8){
                     drawingCanvas.drawText(String.valueOf(compteur),currentPositionX+90 , currentPositionY+120, textPaint);
